fd5a066f71007ebb291c44954f5cc885d10980c7
Willis Aybar <willisaybar@gmail.com>
2020-02-02 19:39:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -15,6 +15,18 @@ require 'pry'
 #     # new_collection << yield(collection)
 #   end
 # end
+
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
   
 
 # def my_collect(lang)
